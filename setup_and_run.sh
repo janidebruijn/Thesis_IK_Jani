b95fbddc6d5812795c1a26e5a1f3db67c88611ed
janidebruijn <j.a.i.de.bruijn@student.rug.nl>
2025-06-08 20:00:02 +0200

    Restructure
    Restructuring all main classification scripts for improving overview

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -12,10 +12,10 @@ module load Python/3.10.4-GCCcore-11.3.0
 module load CUDA/11.7.0
 module load Boost/1.79.0-GCC-11.3.0
 
-source $HOME/venvs/second_env/bin/activate
-#python phi.py
-#python llama.py
-#python qwen.py
-#python binary_phi.py
-#python binary_llama.py
-python binary_qwen.py
+source $HOME/venvs/thesis_env/bin/activate
+#python scripts/phi.py
+#python scripts/llama.py
+#python scripts/qwen.py
+#python scripts/binary_phi.py
+#python scripts/binary_llama.py
+python scripts/binary_qwen.py
